823581c88790605c1f60f61a7acc3b3878ed3116
Sean Conway <81600120+seanway28@users.noreply.github.com>
2021-08-24 14:42:28 -0400

    route/api/index.js

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -7,3 +7,14 @@ const {
     removeReply
 } = require('../../controllers/comment-controller');
 
+// /api/comments/<pizzaId>
+router.route('/:pizzaId').post(addComment);
+
+// /api/comments/<pizzaId>/<commentId>
+router.route('/:pizzaId/:commmentId')
+.put(addReply)
+.delete(removeComment);
+
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
+
+module.exports = router;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,1 +1,11 @@
 const router = require('express').Router();
+const commentRoutes = require('./comment-routes');
+const pizzaRoutes = require('./pizza-routes');
+
+// Add the prefix of `/pizzas` to created in `pizza-routes.js`
+router.use('comments, commentRoutes');
+router.use('/pizzaRoutes');
+
+
+
+module.exports = router;
