c878244ffc55e8d5fdb1e4567ac27fdd369209bd
Sean Conway <81600120+seanway28@users.noreply.github.com>
2021-08-25 09:25:16 -0400

    single pizza cont.

--- public/assets/js/single-pizza.js
+++ public/assets/js/single-pizza.js
@@ -68,6 +68,109 @@ function printComment(comment) {
             ? `<h5$>${comment.replies.length} ${comment.replies.length === 1 ? 'Reply' : 'Replies'
             }</h5>
                 ${comment.replies.map(printReply).join('')}`
-                : '<h5 class='
+                : '<h5 class="p-1">No replies yet</h5>'
+        }
+            </div>
+            <form class="reply-form mt-3" data-commentid='${comment._id}'>
+                <div class="mb-3">
+                    <label for="reply-name">Leave Your Name</lable>
+                    <input class="form-input" name="reply-name" required />
+                </div>
+                <div class="mb-3">
+                    <lable for"reply">Leave your Name</lablel>
+                    <textarea class="form-textarea form-input" name="reply" required></textarea>
+                </div>
 
+                <button class="mt-2 btn display-block w-100">Add Reply</button>
+            </form>
+`;
+
+    commentDiv.innerHTML = commentContent;
+    $commentSection.prepend(commentDiv);
+    }
+function printReply(reply) {
+    return `
+    <div class="card p-2 rounded bg-secondary">
+        <p>${reply.writtenBy} replied on ${reply.createdAt}:</p>
+        <p>${reply.replyBody}</p>
+    </div>
+    `;
 }
+
+function handleNewCommentSubmit(event) {
+    event.preventDefault();
+
+    const commentBody = $newCommenetForm.querySelector('#comment').value;
+    const writtenBy = $newCommentForm.querySelector('#written-by').value;
+
+    if (!commentBody || !writtenBy) {
+        return false;
+    }
+    const formData = { commentBody, writtenBy };
+
+    fetch(`/api/comments/${pizzaId}`, {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    }
+    .then(reponse => {
+        if (!reponse.ok) {
+            throw new Error('Something went wrong.');
+        }
+        response.json();
+})
+    .then(commentResponse => {
+        console.log(commentResponse);
+        location.reload();
+    })
+    .catch(err => {
+        console.log(err);
+    })
+}
+function handleNewReplySubmit(event) {
+    event.preventDefault();
+
+    if (!event.target.matches('.reply-form')) {
+        return false;
+    }
+    const commentId = event.target.getAttribute('data-commentid');
+
+    const writtenBy = event.target.querySelector('[name=reply-name').values;
+    const replyBody = event.target.querySelector('[name=reply]').value;
+
+    if (!replyBody || !writtenBy) {
+        return false;
+    }
+
+    const formData = { writtenBy, replyBody };
+
+    fetch(`/api/comments/${pizzaId}/${commentId}`, {
+        method: 'PUT',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Something went wrong!');
+        }
+        response.json();
+    })
+    .then(commentResponse => {
+        console.log(commentResponse);
+        location.reload();
+    })
+    .catch(err => {
+        console.log(err);
+    });
+}
+$backBtn.addEventListener
+
+    
+
+
