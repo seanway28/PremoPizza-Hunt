cb0dea72f649b32f2dd844c63b9e3f1730596fdf
Sean Conway <81600120+seanway28@users.noreply.github.com>
2021-08-25 11:35:33 -0400

    models redo

---
+++ models/Comment.js
@@ -0,0 +1,63 @@
+const { Schema, model, Types } = require('mongoose');
+const dataFormat = require('../utils/dataFormat');
+
+const ReplySchema = new Schema(
+    {
+        // Set Custom id to avoid confusion with parent comment _id
+        replyId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId() 
+        },
+        replyBody: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        writtenBy: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dataFormat(createdAtVal)
+        }
+    },
+    {   
+        toJSON: {
+            getters: true
+        }    
+    }
+);
+
+const CommentSchema = new Schema ({
+    writtenBy: {
+        type: String,
+        required: "You must provide a user name.",
+        trim: true
+    },
+    commentBody: {
+        type: String,
+        required: "You must provide a comment here.",
+        trim: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    },
+    // Use ReplySchema to validate data for a reply
+    replies: [ReplySchema]
+ 
+},
+	{
+		toJSON: {
+			virtuals: true,
+			getters: true
+		},
+		id: false
+	}
+);
+
+
