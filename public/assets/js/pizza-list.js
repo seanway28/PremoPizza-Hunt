eb9075b48406b9d08524de1f50d3c657499c79dc
Sean Conway <81600120+seanway28@users.noreply.github.com>
2021-08-25 06:32:02 -0400

    pizza list end

--- public/assets/js/pizza-list.js
+++ public/assets/js/pizza-list.js
@@ -13,5 +13,27 @@ const getPizzaList = () => {
 const printPizza = ({ _id, pizzaName, toppings, size, commentCount, createdBy, createdAt}) => {
     const pizzaCard =`
     <div class="col-12 col-llg flex-row">
-    `
-}
+        <div class="caard w-100 flex-column">
+            <h3 class="card-header">${pizzaName}</h3>
+            <div class="card-body flex-column col-auto">
+            <h4 class="text-dark">By ${createdBy}</h4>
+            <p>On ${createdAt}</p>
+            <0>${commentCount} Comments</p>
+            <h5 class="text-dark">Suggested Size: ${size}
+            <ul>
+                ${toppings
+                    .map(topping => {
+                        return `<li>${topping}</li>`;
+                    })
+                    .join('')}
+            </ul>
+            <a class="btn display-block w-100 my-auto" href="/pizza?id=${_id}">See the discussion.</a>
+        </div>
+       </div>
+      </div>
+      `;
+      
+      $pizzaList.innerHTML += pizzaCard;
+};
+
+getPizzaList();
