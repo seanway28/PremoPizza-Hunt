0d7e96f5384c75fc3024308d66e7ac6d62d48c7c
Sean Conway <81600120+seanway28@users.noreply.github.com>
2021-08-24 20:47:30 -0400

    idb

--- public/assets/js/idb.js
+++ public/assets/js/idb.js
@@ -1,3 +1,5 @@
+const { get } = require("../../../routes/api/comment-routes");
+
 // Create variable to hold the db connection
 let db;
 // Establish a connnection to IndexedDB database named 'piiza_hunt' and set to version #1
@@ -42,4 +44,40 @@ function uploadPizza() {
     // Access your object store
     const pizzaObjectStore = transaction.pizzaObjectStore('new_pizza');
     //Get all records from store and set to a variable
-}
+    const getAll = pizzaObjectStore.getAll();
+    // When succesful .getAll() execution, run the following fuction
+    getAll.onsuccess = fuction();
+        //If there was data in indexeddb's store, send it to the api server
+        if(getAll.results.length > 0) {
+            fetch('/api/pizzas', {
+                method: 'POST',
+                body: JSON.stringify(getAll.results),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+            }
+        })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(severResponse);
+                    }
+                    // Open one more transaction
+                    const tranaction = db.tranaction(['new_pizza'], 'readwrite');
+                    // Access the new_pizza object store
+                    const pizzaObjectStore = transaction.objectStore('new_pizza');
+                    // Clear all items in your store
+                    pizzaObjectStore.clear();
+
+                    alert('All saved pizza has been submitted.');
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+            }
+        };
+        // Listen for app coming back online
+        windown.addEventListener('online', uploadPizza);
+                        
+                    
+
